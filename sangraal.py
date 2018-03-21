c53c10e6d3582e0a40b507c767d13886b66cde19
Johan <johan@templar.ventures>
2018-03-20 23:06:43 -0700

    v0.0.1

--- sangraal.py
+++ sangraal.py
@@ -3,23 +3,42 @@ import csv
 import time
 import requests
 
-field_names = ['time', 'last', 'vwap', 'high', 'low', 'open', 'vol']
+field_names = ['BTC', 'ETH', 'LTC', 'XRP', 'STR', 'XMR', 'DASH', 'BTS', 'MAID', 'FCT', 'DOGE', 'CLAM']
 csv_file = open('data.csv', 'w')
 
 writer = csv.DictWriter(csv_file, fieldnames=field_names)
 writer.writeheader()
 
 while True:
-    bitstamp = requests.get('https://www.bitstamp.net/api/ticker/').json()
-    poloniex = requests.get('https://www.bitstamp.net/api/ticker/').json()
     
-    writer.writerow({'time': bitstamp['timestamp'],
-                     'last': bitstamp['last'],
-                     'vwap': bitstamp['vwap'],
-                     'high': bitstamp['high'],
-                     'low': bitstamp['low'],
-                     'open': bitstamp['open'],
-                     'vol': bitstamp['volume']})
+    poloniex = requests.get('https://poloniex.com/public?command=returnTicker').json()
+    
+    BTC = poloniex["USDT_BTC"]
+    ETH = poloniex["BTC_ETH"]
+    LTC = poloniex["BTC_LTC"]
+    XRP = poloniex["BTC_XRP"]
+    STR = poloniex["BTC_STR"]
+    XMR = poloniex["BTC_XMR"]
+    DASH = poloniex["BTC_DASH"]
+    BTS = poloniex["BTC_BTS"]
+    MAID = poloniex["BTC_MAID"]
+    FCT = poloniex["BTC_FCT"]
+    DOGE = poloniex["BTC_DOGE"]
+    CLAM = poloniex["BTC_CLAM"]
+    
+    writer.writerow({'BTC': BTC["percentChange"],
+                     'ETH': ETH["percentChange"],
+                     'LTC': LTC["percentChange"],
+                     'XRP': XRP["percentChange"],
+                     'STR': STR["percentChange"],
+                     'XMR': XMR["percentChange"],
+                     'DASH': DASH["percentChange"],
+                     'BTS': BTS["percentChange"],
+                     'MAID': MAID["percentChange"],
+                     'FCT': FCT["percentChange"],
+                     'DOGE': DOGE["percentChange"],
+                     'CLAM': CLAM["percentChange"],})
+    
     csv_file.flush()
     os.fsync(csv_file.fileno())
     time.sleep(60)
